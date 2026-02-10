e576c15ddd0285c457cb98b485d6fe464d473a9e
Yuhao <hello@chenyuhao.me>
2026-02-09 16:33:24 -0800

    host allow

--- playground/web/vite.config.ts
+++ playground/web/vite.config.ts
@@ -3,5 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: 'all'
+	},
+	preview: {
+		allowedHosts: 'all'
+	}
 });
